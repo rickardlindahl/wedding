5471065907d80eb8aa77bdb950496f3356ac8000
Rickard Lindahl <rickard.lindahl@realsprint.com>
2020-04-01 14:44:06 +0200

    Text fixes and add time on friday

--- src/components/content/friday.tsx
+++ src/components/content/friday.tsx
@@ -28,10 +28,14 @@ const Friday: React.FC = () => {
     >
       <p>
         Vi vill umgås med er så mycket som möjligt under denna helg! Därför
-        träffas de som vill och kan under fredagkväll på Scandic Plaza Umeå för
-        drinkar och härligt surr.
-        <br />
-        <br />
+        träffas de som vill under fredagkväll på Scandic Plaza Umeå för drinkar
+        och härligt surr.
+      </p>
+      <br />
+      <p>20.00</p>
+      <p>Scandic Plaza Umeå</p>
+      <br />
+      <p>
         Anmäl om du vill delta till toastparet på <EmailLink />.
       </p>
     </ContentBlock>
