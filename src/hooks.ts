931b554590082e2913a61b242e78a7e41501b9a5
Rickard Lindahl <rickard.lindahl@realsprint.com>
2020-04-01 21:50:03 +0200

    Remove state from hook using window as value

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,16 +2,12 @@ import { useEffect, useState } from "react"
 import debounce from "lodash/debounce"
 
 export const useWindowHeight = (delay = 500) => {
-  const [height, setHeight] = useState(window.innerHeight)
-
   useEffect(() => {
     const handleResize = () => {
       const newHeight = window.innerHeight
 
       const vh = newHeight * 0.01
       document.documentElement.style.setProperty("--vh", `${vh}px`)
-
-      setHeight(newHeight)
     }
 
     const debouncedHandleResize = debounce(handleResize, delay)
@@ -22,6 +18,4 @@ export const useWindowHeight = (delay = 500) => {
       window.removeEventListener("resize", debouncedHandleResize)
     }
   }, [])
-
-  return height
 }
