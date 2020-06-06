79c4b141fa14a82213a976f1d67f87a86a3487e8
Rickard Lindahl <mail@rickardlindahl.com>
2020-06-06 12:38:37 +0200

    Update checklist

--- src/components/checklist.tsx
+++ src/components/checklist.tsx
@@ -8,51 +8,41 @@ import EmailLink from "./email-link"
 import Separator from "./separator"
 import "./checklist.css"
 
-const renderListItem = (children: React.ReactNode) => (
-  <li className="Checklist-list-item">{children}</li>
-)
-
 const Checklist: React.FC = () => (
   <div id="checklist" className="Checklist">
     <div className="Checklist-inner">
       <h2 className="Checklist-title">Checklista inför bröllopet</h2>
       <Separator />
-      <p>Vill du dela denna dag med oss?</p>
-      <br />
       <p>Kom ihåg att:</p>
       <div className="Checklist-list-container">
         <ol className={classNames("Checklist-list", "Checklist-list--ordered")}>
-          {renderListItem(
-            <>
-              Anmäla dig/er genom att betala in{" "}
-              <Link to={createLinkFromId(coverChargeId)}>kuvertavgiften</Link>
-            </>
-          )}
-          {renderListItem(
-            <>
-              Maila toastparet (<EmailLink />) med information om du/ni:
-              <ul
-                className={classNames(
-                  "Checklist-list",
-                  "Checklist-list--unordered"
-                )}
-              >
-                {renderListItem("behöver någon specialkost")}
-                {renderListItem(
-                  "vill åka med i bussen mellan Umeå och Baggböle Herrgård"
-                )}
-                {renderListItem("vill hålla tal eller spex på bröllopsfesten")}
-                {renderListItem("vill delta på fredagskvällen")}
-                {renderListItem("vill delta på lunch på söndagen")}
-              </ul>
-            </>
-          )}
-          {renderListItem(
-            <>
-              <Link to={createLinkFromId(accomodationsId)}>Boka hotellrum</Link>{" "}
-              - vi har möjlighet att erbjuda hotellrum för 650 kr / natt
-            </>
-          )}
+          <li className="Checklist-list-item">
+            Maila toastparet (<EmailLink />) med information om du/ni:
+            <ul
+              className={classNames(
+                "Checklist-list",
+                "Checklist-list--unordered"
+              )}
+            >
+              <li className="Checklist-list-item">behöver någon specialkost</li>
+              <li className="Checklist-list-item">
+                vill åka med i bussen mellan Umeå och Baggböle Herrgård
+              </li>
+              <li className="Checklist-list-item">
+                vill hålla tal eller spex på bröllopsfesten
+              </li>
+              <li className="Checklist-list-item">
+                vill delta på fredagskvällen
+              </li>
+              <li className="Checklist-list-item">
+                vill delta på lunch på söndagen
+              </li>
+            </ul>
+          </li>
+          <li className="Checklist-list-item">
+            <Link to={createLinkFromId(accomodationsId)}>Boka hotellrum</Link> -
+            vi har möjlighet att erbjuda hotellrum för 650 kr / natt
+          </li>
         </ol>
       </div>
     </div>
